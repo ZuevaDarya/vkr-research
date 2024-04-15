1b11417788d49a7b9dc81269e435c4ef3e7df6e4
ZuevaDarya <zuevadasha55@gmail.com>
2024-04-15 15:01:10 +0300

    добавила подсказку с инструкцией по запуску тестов

--- src/components/canvas-container.js
+++ src/components/canvas-container.js
@@ -1,9 +1,10 @@
 import resultsContainer from './results-container';
+import infoContainer from './info-container';
 
 const canvasContainer = `
   <div class="canvas-container">
     <canvas id="canvas" class="canvas"></canvas>
-
+    ${infoContainer}
     ${resultsContainer}
   </div>
 `;
